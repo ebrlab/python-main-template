0e8a7cbdda3699c5d01f0632d8a2108b758639f8
Etienne Roesch <e.b.roesch@reading.ac.uk>
2020-02-12 15:15:36 +0000

    Added a way of calcultating average

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def main():
     """ Main program """
     # Code goes over here.
     print("Hello world!")
+    
+    
+    first_grade = int( input("Please, enter the first grade: "))
+    second_grade = int( input(".. the second grade: " ))
+    third_grade = int( input(".. the third grade: "))
+    fourth_grade = int( input(".. the fourth grade: "))
+    fifth_grade = int( input(".. the fifth grade: ") )
+    
+    average_grade = (first_grade + second_grade + third_grade + fourth_grade + fifth_grade)/5
+    
+    print("Your average grade is:", average_grade)
     return 0
 
 if __name__ == "__main__":
