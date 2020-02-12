ac0fa59dd53da2af1bf7a8937da50c95f4bb0b2b
Etienne B. Roesch <etienne.roesch@gmail.com>
2020-02-12 11:42:48 +0000

    Initial commit.

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    """ Main program """
+    # Code goes over here.
+    print("Hello world!")
+    return 0
+
+if __name__ == "__main__":
+    main()
